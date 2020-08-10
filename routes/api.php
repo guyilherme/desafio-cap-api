728c2bc338d34ffb5ce6d9bd7af875474785ecd5
Guilherme Macedo da Cruz <gmcaaa@gmail.com>
2020-08-09 22:40:25 -0300

    Rotas

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware('auth:api')->group(function () {
+    Route::get('user', 'UserController@index');
+    Route::get('user/{id}', 'UserController@show');
+
+    Route::get('conta', 'ContaController@index');
+    Route::get('conta/{id}', 'ContaController@show');
+    Route::put('conta/deposito/{id}', 'ContaController@deposito');
+    Route::put('conta/saque/{id}', 'ContaController@saque');
 });
